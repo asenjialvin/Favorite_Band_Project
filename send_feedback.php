e53e93d22e8f50a4b5f701f74a815e20b2e7ac71
asenjialvin <asenjialvin@gmail.com>
2023-11-18 00:08:04 +0300

    updated and debuged the send_feedback.php file

---
+++ send_feedback.php
@@ -0,0 +1,16 @@
+<?php
+    // Replace with your email address
+    $recipientEmail = "asenjialvin@gmail.com";
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $emailBody = "From: $name ($email)\n\nSubject: $subject\n\nMessage:\n$message";
+
+    if (mail($recipientEmail, $subject, $emailBody)) {
+        echo json_encode(["status" => "success"]);
+    } else {
+        echo json_encode(["status" => "error", "message" => "Unable to send email."]);
+    }
